eb5cb280afbddd1ce86a187513c1a14fabccd3d5
Liliia <befolife@gmail.com>
2020-10-28 19:37:02 +0300

    Реализовала menu на JS

---
+++ source/js/menu.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let header = document.querySelector('.site-header');
+let menu = header.querySelector('.site-menu');
+let menuButton = header.querySelector('.site-header__btn-menu');
+let promoWrap = header.querySelector('.promo__wrapper');
+let logoWrapper = header.querySelector('.site-header__logo-wrapper');
+let buttonMenu = header.querySelector('.site-header__btn-menu');
+
+let hideMenu = function () {
+  menu.classList.toggle('site-menu--hide');
+  promoWrap.classList.toggle('promo__wrapper--indents');
+  logoWrapper.classList.toggle('site-header__logo-wrapper--indent');
+  buttonMenu.classList.add('site-header__btn-menu--open');
+};
+
+hideMenu();
+
+let buttonMenuOnClick = function () {
+  hideMenu();
+  buttonMenu.classList.toggle('site-header__btn-menu--close');
+};
+
+buttonMenu.addEventListener('click', buttonMenuOnClick);
