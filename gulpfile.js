6b33b39152bb0828b2bf65e997dbcba1e2cb6f75
Liliia <befolife@gmail.com>
2020-10-29 06:18:50 +0300

    Внесла правки

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,6 @@ var rename = require('gulp-rename');
 var imagemin = require('gulp-imagemin');
 var webp = require('gulp-webp');
 var svgstore = require('gulp-svgstore');
-// var svgclean = require("remove-svg-properties").stream;
-// var html = require("gulp-posthtml");
-// var include = require("posthtml-include");
 var clean = require('del');
 var htmlmin = require('gulp-htmlmin');
 var jsmin = require('gulp-uglify');
@@ -37,8 +34,6 @@ gulp.task('css', function () {
     .pipe(gulp.dest('build/css'))
     .pipe(server.stream());
 });
-
-// gulp.task("start", gulp.series("css", "server"));
 
 gulp.task('images', function () {
   return gulp.src('source/img/*.{png,jpg,svg}')
@@ -89,19 +84,6 @@ gulp.task('js', function () {
   );
 });
 
-// .pipe(rename(function (path) {
-//   path.basename += ".min";
-// }))
-// .pipe(gulp.dest("build/js"));
-
-// gulp.task("html", function () {
-//   return gulp.src("source/*.html")
-//     .pipe(html([
-//       include()
-//     ]))
-//     .pipe(gulp.dest("build"));
-// });
-
 gulp.task('clean', function () {
   return clean('build');
 });
@@ -121,17 +103,12 @@ gulp.task('copy', function () {
 gulp.task('server', function () {
   server.init({
     server: 'build/'
-    // notify: false,
-    // open: true,
-    // cors: true,
-    // ui: false
   });
 
   gulp.watch('source/sass/**/*.scss', gulp.series('css'));
   gulp.watch('source/img/icon-*.svg', gulp.series('sprite'));
   gulp.watch('source/js/*.js', gulp.series('js', 'refresh'));
   gulp.watch('source/*.html', gulp.series('htmlmin', 'refresh'));
-  // gulp.watch("source/*.html", gulp.series("htmlmin", "refresh"));
   // gulp.watch("source/*.html").on("change", server.reload);
 });
 
@@ -140,6 +117,5 @@ gulp.task('refresh', function (done) {
   done();
 });
 
-// gulp.task('build', gulp.series('clean', 'copy', 'css', 'sprite', 'js', 'htmlmin'));
 gulp.task('build', gulp.series('clean', 'copy', 'css', 'sprite'));
 gulp.task('start', gulp.series('build', 'server'));
